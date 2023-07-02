251f405d64ce45a552f65dc146719310cc32a37e
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-02 17:54:13 +0700

    update

---
+++ SuDungToanTu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuDungToanTu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width, hight;
+            Console.Write("Enter Width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter Hight: ");
+            hight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area is: " + width * hight);
+        }
+    }
+}
