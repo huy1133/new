5cc29431e666bfaecd8756bf7b14e045a53e1f15
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-02 17:47:26 +0700

    update

---
+++ systime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace systime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.InputEncoding = Encoding.UTF8;
+            //Console.OutputEncoding = Encoding.UTF8;
+            DateTime localdate = DateTime.Now;
+            Console.WriteLine("DateTime now is: "+ localdate);
+        }
+    }
+}
