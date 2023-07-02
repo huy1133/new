08fb4c75ea03a790f1eab02101a2bde3f71bdb40
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-02 18:01:13 +0700

    update

---
+++ ChuyenDoiTienTe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuyenDoiTienTe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tigia = 23000;
+            Console.Write("Nhap So USD: ");
+            int usd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Gia Tri VND: " + tigia * usd+ " VND");
+        }
+    }
+}
