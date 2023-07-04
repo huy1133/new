04c10a7444006325949f14172cfc0ab06ebfe711
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-04 19:44:07 +0700

    update

---
+++ Body mass index-BMI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body_mass_index_BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height, weight;
+            Console.WriteLine("Please enter a height");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight");
+            weight = float.Parse(Console.ReadLine());
+
+            double BMI = weight / Math.Round(height,2);
+
+            if (BMI < 18.5)
+                Console.WriteLine("Underweight");
+            else if (BMI < 25)
+                Console.WriteLine("Normal");
+            else if (BMI < 30)
+                Console.WriteLine("Overweight");
+            else Console.WriteLine("Obese");
+        }
+    }
+}
