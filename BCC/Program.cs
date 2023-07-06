06685abc8b25769975eb95c5197ffabb65bd5ac9
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-06 21:03:01 +0700

    update

---
+++ BCC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=2; i<=10; i++)
+            {
+                Console.WriteLine("Bang Cuu Chung {0}:",i);
+                for(int j=1; j<=10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
+                }
+            }
+        }
+    }
+}
---
+++ UCLN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCLN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(UCLN(a,b));
+        }
+        static int UCLN(int n, int m)
+        {
+            if (n == m) return n;
+            return n > m ? UCLN(n - m, m) : UCLN(n, m - n);
+        }
+    }
+}
