65a9ad6934161dacf8a882f1f5deb31f01942c1c
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-07 13:35:37 +0700

    update

---
+++ ThuNghiem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuNghiem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3);
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine(55);
+        }
+    }
+}
---
+++ TinhTongCacPhanTuTrongMang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTongCacPhanTuTrongMang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            int total = 0;
+            foreach(int i in numbers)
+            {
+                total += i;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
