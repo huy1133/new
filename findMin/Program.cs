7e75c2fe98698b4f5bf99e4ac923e2b06b0b0952
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-13 14:20:46 +0700

    update

---
+++ findMin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findMin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=10;
+            int[] arr = new int[n];
+            Create(arr);
+        }
+        public static void Create(int[] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1,20);
+            }
+            Console.WriteLine("So Nho Nhat La {0} O Vi Tri {1}", arr[findmin(arr)], findmin(arr));
+        }
+        public static int findmin(int[] arr)
+        {
+            int index = 0;
+            for (int i = 0;i < arr.Length; i++)
+            {
+                if (arr[i] < arr[index])
+                    index = i;
+            }
+            return index;
+        }
+    }
+}
