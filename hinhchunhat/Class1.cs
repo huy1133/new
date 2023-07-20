5f1e29f8c76e384194fd2151dd676699a9d88bfe
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-20 12:54:48 +0700

    add

---
+++ hinhchunhat/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hinhchunhat
+{
+    internal class Rectangle
+    {
+        private double width, height;
+        public Rectangle(double weight = 0, double height = 0)
+        {
+            this.width = weight;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return this.height*this.width;
+        }
+        public double GetPerimeter()
+        {
+            return (this.height+this.width)*2;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0}:{1}", this.width, this.height);
+        }
+    }
+}
---
+++ hinhchunhat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hinhchunhat
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double height, width;
+            Console.Write("Enter Width: ");
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width,height);
+            rectangle.Display();
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+        }
+    }
+}
