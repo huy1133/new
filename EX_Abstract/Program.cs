35ac483399d64dea5aef83d587fbbb18e39208dd
tranvanhuy <vanhuy26032003@gmail.com>
2023-07-21 15:05:56 +0700

    add

---
+++ EX_Abstract/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_Abstract
+{
+    abstract class Shap
+    {
+        public const double pi = Math.PI;
+        public double _x, _y;
+        public Shap(double x, double y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+        public abstract double circle_area();
+    }
+    class Circle: Shap
+    {
+        public Circle(double radius)
+            :base(radius,0)
+        {
+            
+        }
+        public override double circle_area()
+        {
+            return pi*_x*_x;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ [Thực hành] lớp Animal và interface Edible/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Thực_hành__lớp_Animal_và_interface_Edible
+{
+    abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+    interface Edible
+    {
+        string HowToEat();
+    }
+    class Tiger: Animal
+    {
+        public override string MakeSound()
+        {
+            return "Grmmmmmm";
+        }
+    }
+    class Chicken:Animal, Edible
+    {
+        public override string MakeSound()
+        {
+            return "cuc cuc tac";
+        }
+
+        public string HowToEat()
+        {
+            return "could be friend ";
+            //throw new NotImplementedException();
+        }
+    }
+    abstract class Fruit: Edible
+    {
+        public abstract string HowToEat();
+    }
+    class orange: Fruit
+    {
+        public override string HowToEat()
+        {
+            return "orange can be slider";
+            //throw new NotImplementedException();
+        }
+        
+    }
+    class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple can be slider";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple();
+            Console.WriteLine(apple.HowToEat());
+        }
+    }
+}
